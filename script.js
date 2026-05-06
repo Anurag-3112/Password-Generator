1a2eb1b5f896341cd63c3c4f00d93d4562c7ef3a
Anurag-3112 <ak7546824@gmail.com>
2026-05-06 16:28:25 +0530

    project

---
+++ script.js
@@ -0,0 +1,25 @@
+function generatePassword() {
+const length = document.getElementById("length").value;
+const hasUpper = document.getElementById("uppercase").checked;
+const hasLower = document.getElementById("lowercase").checked;
+const hasNumber = document.getElementById("numbers").checked;
+const hasSymbol = document.getElementById("symbols").checked;
+
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*()_+[]{}";
+
+let chars = "";
+if (hasUpper) chars += upper;
+if (hasLower) chars += lower;
+if (hasNumber) chars += number;
+if (hasSymbol) chars += symbol;
+
+let password = "";
+for (let i = 0; i < length; i++) {
+password += chars[Math.floor(Math.random() * chars.length)];
+}
+
+document.getElementById("password").value = password;
+}
